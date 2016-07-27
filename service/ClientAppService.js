960b3b80ed124c828e0425e130700e2c42478a47
shivam4dx <shivam.rohilla@4dxfitness.com>
2016-07-27 19:18:25 +0530

    dsdasd

--- service/ClientAppService.js
+++ service/ClientAppService.js
@@ -1,4 +1,4 @@
-angular.module('shivamTestSprint7RefactorApp')
+angular.module('testServiceModule', [])
   .service('ClientAppService', function () {
       this.call = function () {
         console.log('In ClientAppService :: methodName :: call');
