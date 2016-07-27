0bc9797699342894e3d14f2bc7d3e9e200043b7a
shivam4dx <shivam.rohilla@4dxfitness.com>
2016-07-27 19:20:59 +0530

    reverting the module name

--- service/ClientAppService.js
+++ service/ClientAppService.js
@@ -1,4 +1,4 @@
-angular.module('testServiceModule', [])
+angular.module('shivamTestSprint7RefactorApp')
   .service('ClientAppService', function () {
       this.call = function () {
         console.log('In ClientAppService :: methodName :: call');
